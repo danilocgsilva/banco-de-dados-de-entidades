b073d8cb7268356597835ec227f76d5764931347
Danilo Silva <contato@danilocgsilva.me>
2023-07-06 12:29:46 -0300

    Working first api version

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-let http = require("http");
+import http from "http"
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type':'application/json'});
+const getDataFromApi = async () => {
 
     const apiAddress = "https://www.4devs.com.br/ferramentas_online.php"
-    const verb = "POST"
-    const querydata = "acao=gerar_pessoa&sexo=I&pontuacao=S&idade=0&cep_estado=&txt_qtde=1&cep_cidade="
 
-    const response = {
-        "key": "John Doe"
+    var form = new FormData();
+    form.append('acao', "gerar_pessoa")
+    form.append('sexo', 'I')
+    form.append('pontuacao', 'S')
+    form.append('txt_qtde', '1')
+
+    const responseFromApi = await fetch(
+        apiAddress,
+        {
+            method: verb,
+            body: "POST"
+        }
+    )
+
+    if (responseFromApi.ok) {
+        return await responseFromApi.json()
     }
-    
-	res.end(JSON.stringify(response));
-}).listen(3000, "0.0.0.0");
 
-console.log("Servidor de http em node rodando. Acesse 0.0.0.0, porta 3000");
+    throw new Error("A bad thing happened...")
+}
+
+http.createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const contentToResponde = await getDataFromApi()
+    res.end(JSON.stringify(contentToResponde));
+}).listen(3000, "0.0.0.0");
